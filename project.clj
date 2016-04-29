9e8b8893051509641f5b60bc4c967f11eebd6c07
Doug Knight <doug.knight@livingsocial.com>
2016-04-29 13:27:51 -0500

    add lein-copy-deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.owasp/dependency-check-core "1.3.6"]
-                 [org.owasp/dependency-check-utils "1.3.6"]]
+                 [org.owasp/dependency-check-utils "1.3.6"]
+                 [lein-copy-deps "0.1.0"]]
   :eval-in-leiningen true)
