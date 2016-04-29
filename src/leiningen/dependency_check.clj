dcad99c9e0860d7e699b10aa5f762edffa30b696
Doug Knight <doug.knight@livingsocial.com>
2016-04-29 13:38:03 -0500

    finish writing report (XML only)

--- src/leiningen/dependency_check.clj
+++ src/leiningen/dependency_check.clj
@@ -1,24 +1,69 @@
 (ns leiningen.dependency-check
   (:require [clojure.java.io :as io]
-	        [leiningen.core.classpath :as cp])
+            [leiningen.core.classpath :as cp]
+            [leiningen.core.main :refer [info warn debug]]
+            [clojure.pprint :refer [pprint]])
   (:import (org.owasp.dependencycheck Engine)
+           (org.owasp.dependencycheck.utils Settings)
            (org.owasp.dependencycheck.data.nvdcve CveDB)
-		   (org.owasp.dependencycheck.data.lucene LuceneUtils)
-		   (org.owasp.dependencycheck.dependency Dependency)
-		   (org.owasp.dependencycheck.dependency Identifier)
-		   (org.owasp.dependencycheck.dependency Vulnerability)
-		   (org.owasp.dependencycheck.reporting ReportGenerator)
-		   (org.owasp.dependencycheck.utils Settings)))
+           (org.owasp.dependencycheck.reporting ReportGenerator ReportGenerator$Format)))
+
+(defn- db-properties
+  "Returns the CVE database properties"
+  []
+  (let [cve-db (CveDB.)
+        _ (.open cve-db)
+        result (.getDatabaseProperties cve-db)
+        _ (.close cve-db)]
+    result))
+
+(defn- target-files
+  "Selects the files to be scanned"
+  [project]
+  (->> (cp/get-classpath project)
+       (filter (partial re-find #"\.jar$"))
+       (map io/file)))
+
+(defn- scan-files
+  "Scans the specified files and returns the engine used to scan"
+  [files]
+  (Settings/initialize)
+  (let [engine (Engine.)]
+    (info "Scanning" (count files) "file(s)...")
+    (doseq [file files]
+      (info "Scanning file" (.getCanonicalPath file))
+      (.scan engine file))
+    (info "Done.")
+
+    engine))
+
+(defn- analyze-files
+  "Analyzes the files scanned by the specified engine and returns the engine"
+  [engine]
+  (info "Analyzing dependencies...")
+  (.analyzeDependencies engine)
+  (info "Done.")
+
+  engine)
+
+(defn- write-report
+  "Writes a report using the analysis information in the specified engine and returns the engine"
+  [engine]
+  (info "Generating report...")
+  (let [generator (ReportGenerator. "Analysis Report"
+                                    (.getDependencies engine)
+                                    (.getAnalyzers engine)
+                                    (db-properties))]
+    (.generateReports generator "target" ReportGenerator$Format/HTML))
+  (info "Done.")
+
+  engine)
 
 (defn dependency-check
-  "I don't do a lot."
+  "Scans the JAR files found on the class path and writes a report to the ./target directory"
   [project & args]
-  (Settings/initialize)
-  (let [engine (Engine.)
-	     jars (map io/file (filter (partial re-find #"\.jar$") (cp/get-classpath project)))]
-		 (CpeMemoryIndex/getInstance)
-    (doseq [jar jars]
-		(println "Scanning" (.getPath jar) (.getName jar))
-		(.scan engine jar)
-		(.analyzeDependencies engine)))
-  (println "Hi!"))
+  (-> project
+      target-files
+      scan-files
+      analyze-files
+      write-report))
