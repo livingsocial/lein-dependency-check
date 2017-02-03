bcf7b132f18091f4ce5cdb8dba9cc3ef22c6381d
Doug Knight <doug.knight@livingsocial.com>
2017-02-03 09:01:07 -0600

    update project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.livingsocial/lein-dependency-check "0.2.0-SNAPSHOT"
+(defproject com.livingsocial/lein-dependency-check "0.2.0"
   :description "Clojure command line tool for detecting vulnerable project dependencies"
   :url "https://github.com/livingsocial/lein-dependency-check"
   :license {:name "The MIT License (MIT)"
