fea0bd46289b8c44da500984deabad7275703c4f
Doug Knight <doug.knight@livingsocial.com>
2017-02-16 09:49:52 -0600

    remove snapshot from version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.livingsocial/lein-dependency-check "0.2.1-SNAPSHOT"
+(defproject com.livingsocial/lein-dependency-check "0.2.1"
   :description "Clojure command line tool for detecting vulnerable project dependencies"
   :url "https://github.com/livingsocial/lein-dependency-check"
   :license {:name "The MIT License (MIT)"
