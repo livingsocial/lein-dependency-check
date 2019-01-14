720765eac2026506ab3c41ae70cab38eac5917d0
Cayvon Hamidizadeh <cayvon.hamidizadeh@nasa.gov>
2019-01-14 18:12:27 -0500

    Fix default overwritting config.

--- src/leiningen/dependency_check.clj
+++ src/leiningen/dependency_check.clj
@@ -11,12 +11,17 @@
    [nil "--throw" "throw error when vulnerabilities found"]
    ["-p" "--properties-file FILE" "Specificies a file that contains properties to merge with defaults."]
    ["-f" "--output-format FORMAT(S)" "The output format to write to (XML, HTML, CSV, JSON, VULN, ALL). Default is HTML"
-    :default ["html"]
     :parse-fn (fn [output-format]
                 (-> (string/replace output-format #":" "")
                     (string/split #",")))]
-   ["-o" "--output-directory DIR" "The folder to write to. The default is ./target"
-    :default "target"]])
+   ["-o" "--output-directory DIR" "The folder to write to. The default is ./target"]])
+
+(def cli-defaults
+  "Default options."
+  {:output-format ["html"]
+   :output-directory "target"
+   :log false
+   :throw false})
 
 (defn- dependency-check-project
   "Create a project to launch dependency-check, with only dependency-check as a dependency."
@@ -41,7 +46,8 @@
   [project & args]
   (let [classpath (cp/get-classpath project)
         name (:name project)
-        config (merge (:dependency-check project)
+        config (merge cli-defaults
+                      (:dependency-check project)
                       (:options (parse-opts args cli-options)))]
     (eval/eval-in-project (dependency-check-project project)
                           `(lein-dependency-check.core/main '~classpath '~name '~config)
