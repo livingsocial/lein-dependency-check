779386603dc3ef63d5fb7005bc4191b89f05d84d
Drew Thompson <drew.thompson@singlewire.com>
2022-10-07 21:30:18 -0500

    upgrade dependency-check-version to 7.2.1
    Signed-off-by: Drew Thompson <drew.thompson@singlewire.com>

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(def dependency-check-version "6.5.3")
+(def dependency-check-version "7.2.1")
 
-(defproject com.livingsocial/lein-dependency-check "1.3.0"
+(defproject com.livingsocial/lein-dependency-check "1.4.0-SNAPSHOT"
   :description "Clojure command line tool for detecting vulnerable project dependencies"
   :url "https://github.com/livingsocial/lein-dependency-check"
   :license {:name "The MIT License (MIT)"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.owasp/dependency-check-core ~dependency-check-version]
                  [org.owasp/dependency-check-utils ~dependency-check-version]
-                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/tools.cli "1.0.206"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/jcl-over-slf4j "1.7.30"]
-                 [org.slf4j/slf4j-api "1.7.32"]
-                 [org.apache.logging.log4j/log4j-api "2.17.1"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.14.1"]
-                 [org.apache.logging.log4j/log4j-core "2.17.1"]]
+                 [org.slf4j/jcl-over-slf4j "2.0.3"]
+                 [org.slf4j/slf4j-api "2.0.3"]
+                 [org.apache.logging.log4j/log4j-api "2.19.0"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.19.0"]
+                 [org.apache.logging.log4j/log4j-core "2.19.0"]]
   :eval-in-leiningen true)
